0986dede1a7512e6cb99add3ba4a08da7a3c89d5
GoogleFrog <googl3frog@gmail.com>
2022-12-01 00:56:12 +1100

    Revert "Blind add 1388 AI deployment (since I need to do zkmenu:test to test it anyway) in case we need to go back."

--- LuaMenu/configs/gameConfig/zk/aiPrefixFunc.lua
+++ LuaMenu/configs/gameConfig/zk/aiPrefixFunc.lua
@@ -1,12 +1,6 @@
 
 local function AiPrefixFunc()
-	if WG.Chobby.Configuration:IsCurrentVersionNewerThan(105, 1380) then
-		return "1051388"
-	end
-	if WG.Chobby.Configuration:IsCurrentVersionNewerThan(105, 900) then
-		return "1051344"
-	end
-	return "105"
+	return ((WG.Chobby.Configuration:IsCurrentVersionNewerThan(105, 900) and "1051344") or "105")
 end
 
 return AiPrefixFunc
