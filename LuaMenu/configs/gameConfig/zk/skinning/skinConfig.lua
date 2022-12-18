160fcba451fb5faafa72ab5521e5f7ae97e98a9f
GoogleFrog <googl3frog@gmail.com>
2022-12-18 13:27:39 +1100

    Added new backgrounds. I'm not keen on having a long-lasting background replacement since people might not even notice it is a replacement if they are new, and it isn't even winter here. Winter bridges christmas and new year as a compromise.  * Christmas (18/12 -> 31/12) -> (16/12 -> 27/12) (twelve days)  * Winter 28/12, 29/12, 30/12  * New year 31/12, 1/1 -> 5/1
    Fixes https://github.com/ZeroK-RTS/Zero-K/issues/4792

--- LuaMenu/configs/gameConfig/zk/skinning/skinConfig.lua
+++ LuaMenu/configs/gameConfig/zk/skinning/skinConfig.lua
@@ -2,12 +2,23 @@ local shortname = "zk"
 local backgroundImage = LUA_DIRNAME .. "configs/gameConfig/" .. shortname .. "/skinning/background.jpg"
 
 local currentTime = os.date('*t')
+
 if currentTime and (tonumber(currentTime.month or 0) == 10 and tonumber(currentTime.day or 0) >= 23) or (currentTime.month == 11 and currentTime.day == 1) then
 	backgroundImage = LUA_DIRNAME .. "configs/gameConfig/" .. shortname .. "/skinning/background_halloween_kingstad.jpg"
 end
 
-if currentTime and (tonumber(currentTime.month or 0) == 12 and tonumber(currentTime.day or 0) >= 18) then
-	backgroundImage = LUA_DIRNAME .. "configs/gameConfig/" .. shortname .. "/skinning/background_christmas.jpg"
+if currentTime and (tonumber(currentTime.month or 0) == 12 and tonumber(currentTime.day or 0) >= 16) then
+	if tonumber(currentTime.day or 0) < 28 then
+		backgroundImage = LUA_DIRNAME .. "configs/gameConfig/" .. shortname .. "/skinning/background_christmas.jpg"
+	elseif tonumber(currentTime.day or 0) < 31 then
+		backgroundImage = LUA_DIRNAME .. "configs/gameConfig/" .. shortname .. "/skinning/background_winter.jpg"
+	else
+		backgroundImage = LUA_DIRNAME .. "configs/gameConfig/" .. shortname .. "/skinning/background_new_year.jpg"
+	end
+end
+
+if currentTime and (tonumber(currentTime.month or 0) == 1 and tonumber(currentTime.day or 0) < 6) then
+	backgroundImage = LUA_DIRNAME .. "configs/gameConfig/" .. shortname .. "/skinning/background_new_year.jpg"
 end
 
 local config = {
