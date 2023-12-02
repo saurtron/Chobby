0e69782586c18a0764dcb4e3a360e64027ae8d46
GoogleFrog <googl3frog@gmail.com>
2023-12-02 14:47:00 +1100

    Try to fix easy circuit update.

--- LuaMenu/configs/gameConfig/zk/aiPrefixFunc.lua
+++ LuaMenu/configs/gameConfig/zk/aiPrefixFunc.lua
@@ -1,6 +1,12 @@
 
 local function AiPrefixFunc()
-	return ((WG.Chobby.Configuration:IsCurrentVersionNewerThan(105, 900) and "1051344") or "105")
+	if WG.Chobby.Configuration:IsCurrentVersionNewerThan(105, 2000) then
+		return "1052188"
+	end
+	if WG.Chobby.Configuration:IsCurrentVersionNewerThan(105, 900) then
+		return "1051344"
+	end
+	return "105"
 end
 
 return AiPrefixFunc
